af626d2a1fd92fb3e8d0a538af9053f1c3159f8b
Müller Csaba <csaba.muller@hotmail.com>
2023-02-19 22:35:22 +0100

    javascript code snippet fully worked!

--- app.js
+++ app.js
@@ -2,16 +2,40 @@ const icon = document.getElementById("share-icon");
 const shareTable = document.querySelector(".share-table.hidden");
 const reverseIcon = document.getElementById("icon-reverse-background");
 const footerElement = document.querySelector("footer");
-
+const mediaQuery = window.matchMedia("(min-width: 1200px)")
 
 icon.addEventListener("click", () => {
+  if (mediaQuery.matches) {
+    shareTable.classList.toggle("hidden");
+  } else {
+    footerElement.style.display = "none";
     shareTable.classList.remove("hidden");
-    footerElement.style.display="none";
-
+  }
 });
-
 
 reverseIcon.addEventListener("click", () => {
-    shareTable.classList.add("hidden")
-    footerElement.style.display="grid";
+  if (!shareTable.classList.contains("hidden")) {
+    shareTable.classList.add("hidden");
+    footerElement.style.display = "grid";
+  }
 });
+
+
+/* A toggle() metódus megváltoztatja az elem osztályát a megadott osztályhoz képest. 
+Tehát, ha az elemnek már van "hidden" osztálya, akkor eltávolítja, ha nincs, akkor hozzáadja. 
+Így a fenti kódban a shareTable osztálya egyszerre jelenik meg és tűnik el az ikonra kattintva, 
+ha az oldal szélessége meghaladja a 1200px-et, és csak eltűnik, ha a "reverseIcon"-ra kattintanak, 
+és a "shareTable" látható volt.
+ */
+
+
+
+//---------------Eredeti kód, ami nem volt tökéletes--------------------//
+
+/* if (mediaQuery.matches) {
+    shareTable.classList.remove("hidden");
+
+} else {
+    footerElement.style.display = "none";
+    shareTable.classList.remove("hidden");
+} */
